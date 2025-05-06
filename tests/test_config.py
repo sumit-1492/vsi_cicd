052d42f53eb4b200c7d167879e01a7dc5432d22d
sumit-1492 <sumitlala99@gmail.com>
2025-05-06 07:23:05 +0530

    app created and tested

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,15 @@
+import pytest
 
-
+class rangeError(Exception):
+    def __init(self,message = "value not in range"):
+        self.message = message
+        super().__init__(self.message)
+        
+        
+        
 def test_generic():
     a = 2
-    b = 2
-    assert a==b
+    with pytest.raises(rangeError):
+        if a not in range(3,10):
+            raise rangeError
+        
